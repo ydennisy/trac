1afaea9f91e58abfd5c6d9b718fa6039bbfe9c46
Dennis <dyurkevich@radiumone.com>
2015-12-03 09:16:00 +0000

    02.12.15

---
+++ app/controllers/ui.client.js
@@ -0,0 +1,39 @@
+"use strict";
+
+(function (){
+    var countApiUrl = "https://trac-ydennisy.c9users.io/api/count";
+    var countSpan = document.getElementById("count-nbr");
+    var getCountButton = document.getElementById("getCount");
+
+    function ready (fn) {
+        if (typeof fn !== "function"){
+            return;
+        }
+        if (document.readyState === "complete"){
+            return fn();
+        }
+        document.addEventListener("DOMContentLoaded", fn, false);
+    }
+    
+    function ajaxRequest (method, url, callback){
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function(){
+            if (xmlhttp.readyState === 4 && xmlhttp.status === 200){
+                callback(xmlhttp.response);
+            }
+        };
+        xmlhttp.open(method, url, true);
+        xmlhttp.send();
+    }
+    
+    function updateCount (data){
+        var impsCount = JSON.parse(data);
+        countSpan.innerHTML = impsCount;
+    }
+    
+    ready(ajaxRequest("GET", countApiUrl, updateCount));
+    
+    getCountButton.addEventListener("click", function(){
+        ajaxRequest("GET", countApiUrl, updateCount);
+    })
+})();
--- server.js
+++ server.js
@@ -3,22 +3,22 @@
 var express = require("express");
 var http = require('http');
 var url = require('url');
+
 var qs = require("querystring");
-var bodyParser = require("body-parser");
 var mongo = require("mongodb").MongoClient;
 var route = require("./app/routes/index.js");
 var app = express();
+var bodyParser = require("body-parser");
 
 mongo.connect("mongodb://localhost:27017/trac", function (err, db){
     if (err) {throw new Error("Connection to db failed"); }
     else {console.log("mongoDB connected successfully on port 27017"); }
 
-    app.use(bodyParser.urlencoded());
-    app.use(bodyParser.json());
+    app.use(bodyParser());
     app.use("/public", express.static(process.cwd() + "/public"));
     app.use("/client", express.static(process.cwd() + "/client"));
     app.use("/script", express.static(process.cwd() + "/script"));
-    app.use(bodyParser.json());
+    app.use("/app", express.static(process.cwd() + "/app"));
 
     route(app, db);
 
