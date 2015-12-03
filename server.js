717b2acbf54f5a898530b8312327d9a74d6e7a68
Dennis <dyurkevich@radiumone.com>
2015-12-03 16:41:21 +0000

    test1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var http = require('http');
 var url = require('url');
-
+var morgan   = require('morgan');
 var qs = require("querystring");
 var mongo = require("mongodb").MongoClient;
 var route = require("./app/routes/index.js");
@@ -19,6 +19,7 @@ mongo.connect("mongodb://localhost:27017/trac", function (err, db){
     app.use("/client", express.static(process.cwd() + "/client"));
     app.use("/script", express.static(process.cwd() + "/script"));
     app.use("/app", express.static(process.cwd() + "/app"));
+    app.use(morgan('dev')); // log every request to the console
 
     route(app, db);
 
