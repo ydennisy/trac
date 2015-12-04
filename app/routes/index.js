56a90b3744bc412f03f83866a3eb883f8f94d511
Dennis <dyurkevich@radiumone.com>
2015-12-04 13:18:54 +0000

    test2

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,7 +18,7 @@ module.exports = function (app, db) {
 
     app.route("/script")
         .get(function(req, res){
-            res.send("<script type='text/javascript' src='"+req.hostname+"/script/trac.js'></script>");
+            res.send("<script type='text/javascript' src='http://"+req.hostname+"/script/trac.js'></script>");
         });
 
     app.route("/api")
