cb18f2c1cfd111aba1368b644d5419e03ce3dfe6
Dennis <dyurkevich@radiumone.com>
2016-02-09 14:59:54 +0000

    bug1

--- app/routes/index.js
+++ app/routes/index.js
@@ -23,7 +23,7 @@ module.exports = function (app, db) {
         
     app.route("/script")
         .get(function(req, res){
-            res.send("<script type='text/javascript' src='/script/trac.js'></script>");
+            res.send("<script type='text/javascript' src='http://ec2-52-18-3-176.eu-west-1.compute.amazonaws.com/script/trac.js'></script>");
         });
 
     app.route("/api")
