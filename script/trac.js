8a2944a35e16ae0ef15d651a4240af1729d9e16f
Dennis <dyurkevich@radiumone.com>
2016-02-14 20:56:16 +0000

    js file

--- script/trac.js
+++ script/trac.js
@@ -1,7 +1,7 @@
 //wrap script to ensure no issues with variable names
 (function(){
    function postStaticData (ts, queryS, cookies, ua, focus, scripts, images, urlloc){
-    var url = "/api";
+    var url = "http://ec2-52-18-3-176.eu-west-1.compute.amazonaws.com/api";
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json;charset=UTF-8");
