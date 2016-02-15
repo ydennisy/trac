bdf226b71dd8389a0347564cc9f2b3c951ca244d
Dennis <dyurkevich@radiumone.com>
2016-02-15 21:18:16 +0000

    js file

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -1,9 +1,6 @@
 "use strict";
-
 function clickHandler (db) {
-
     var imps = db.collection("imps");
-
     // method to return the number of records in the collection "imps"
     this.getCount = function (req, res){
         imps.count({}, function(err, result){
@@ -13,17 +10,15 @@ function clickHandler (db) {
             res.json(result);
         });
     };
-    
+    // method to extract a single record from the DB.
     this.getResult = function (req, res){
         imps.findOne({}, function(err, result){
             if (err) {
                 throw err;
             }
-            res.json(result);
+            res.send(result);
         });
     };
-
-
 }
 
 module.exports = clickHandler;
